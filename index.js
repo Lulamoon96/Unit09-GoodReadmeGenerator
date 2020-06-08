5327300b61511afacc0513641cca1bed752de1c9
Kuroneko-Rares <luigi.galvan@berkeley.edu>
2020-06-08 00:01:21 -0700

    Funcionality worked on.

--- index.js
+++ index.js
@@ -1,28 +1,84 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
 const util = require("util")
+const axios = require("axios")
 
 const appendFileAsync = util.promisify(fs.appendFile)
+const writeFileAsync = util.promisify(fs.writeFile)
 
 inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your user name?",
+      message: "What is your github user name?",
       name: "username"
     },
     {
       type: "input",
-      message: "Put some crap here.",
-      name: "confirm"
+      message: "What is your project called?",
+      name: "name"
+    },
+    {
+        type: "input",
+        message: "Please type in the exact repo of the project if applicable. If not, leave blank.",
+        name: "repo"
+    },
+    {
+      type: "input",
+      message: "Please type in a short description of the project.",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Would you like a table of contents? (true or false)",
+      name: "contents"
     }
   ])
-  .then(function(response) {
+  .then(async function(response) {
 
-    for (let [key, value] of Object.entries(response)) {
+    // console.log(response)
+    // const queryUrl = `https://api.github.com/users/${username}`
+    const { name, description, contents } = response
+
+
+    await writeFileAsync("readme/README.md", `[![made-for-VSCode](https://img.shields.io/badge/Made%20for-VSCode-1f425f.svg)](https://code.visualstudio.com/)` + "\n")
+    await appendFileAsync("readme/README.md", `# ${name}` + "\n")
+    await appendFileAsync("readme/README.md", `${description}` + "\n")
+    .catch(function(err) {
+
+        console.log(err);
+
+    })
     
-        appendFileAsync("readme/test.txt", `${key}: ${value}` + "\n")
+    if (contents === 'true'){
+        
+        await appendFileAsync("readme/README.md", `# Table of Contents` + "\n")
+        await appendFileAsync("readme/README.md", 
+        `
+- [Usage](#usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Test)
+- [Questions](#Question)
+        ` + "\n")
+        .catch(function(err) {
+
+            console.log(err);
     
+        })
+
     }
+    // axios
+    //     .get(queryUrl)
+    //     .then(function(res) {
+
+    //         const { login, avatar_url } = res.data
+
+    //     })
+    //     .catch(function(err) {
+
+    //         console.log(err);
+
+    //     })
 
   })
